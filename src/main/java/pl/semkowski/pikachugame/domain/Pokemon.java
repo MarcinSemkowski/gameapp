48a4d214d7bfb745f450a96d96dda45d0a1299d2
Marcin <marcin96semkowski@gmail.com>
2019-04-27 12:16:39 +0200

    update #2

--- src/main/java/pl/semkowski/pikachugame/domain/Pokemon.java
+++ src/main/java/pl/semkowski/pikachugame/domain/Pokemon.java
@@ -6,24 +6,73 @@ public abstract class Pokemon {
    // private int stamina;
     private int level;
     private int demage;
-   // private int defense;
+    private int energy;
+    private int defense;
    // private boolean hunger;
 
-    public Pokemon(String name, int level, int demage) {
+    public Pokemon(String name, int demage,int energy) {
         this.name = name;
         this.hitPoints = 100;
+        this.level = 1;
+        this.demage = demage;
+        this.energy = energy;
+    }
+
+    protected int getDefense() {
+        return defense;
+    }
+
+    protected void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    protected String getName() {
+        return name;
+    }
+
+    protected void setName(String name) {
+        this.name = name;
+    }
+
+    protected int getHitPoints() {
+        return hitPoints;
+    }
+
+    protected void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    protected int getLevel() {
+        return level;
+    }
+
+    protected void setLevel(int level) {
         this.level = level;
+    }
+
+    protected int getDemage() {
+        return demage;
+    }
+
+    protected void setDemage(int demage) {
         this.demage = demage;
     }
 
+    protected int getEnergy() {
+        return energy;
+    }
+
+    protected void setEnergy(int energy) {
+        this.energy = energy;
+    }
 
     // protected abstract void eat(String food);
 
    // protected abstract int sleep(int time);
 
-    protected abstract int attack();
+    protected abstract int attack(int choice);
 
-    protected abstract void defense();
+    protected abstract int defense(int damage);
 
-
+    protected abstract void levelUP();
 }
--- src/main/java/pl/semkowski/pikachugame/domain/pokemon/ElectricPokemon.java
+++ src/main/java/pl/semkowski/pikachugame/domain/pokemon/ElectricPokemon.java
@@ -6,30 +6,52 @@ import pl.semkowski.pikachugame.type.Electric;
 public class ElectricPokemon extends Pokemon implements Electric {
 
 
-    public ElectricPokemon(String name, int level, int demage, int energy) {
-        super(name, level, demage);
-        this.energy = energy;
+
+
+    public ElectricPokemon(String name) {
+        super(name,  10,100);
+
+
     }
 
-    private int energy;
+
+    @Override
+    protected int attack(int choice) {
+        int damage=0;
+        switch(choice){
+             case 1:
+                 System.out.println("Attack with defense ,damage =5, energy -7");
 
 
-    public int getEnergy() {
-        return energy;
-    }
+                setEnergy(getEnergy() - 5);
 
-    public void setEnergy(int energy) {
-        this.energy = energy;
+                break;
+            case 2:
+                System.out.println("Pure attack , damage = 10 ,");
+
+                setEnergy(getEnergy() - 5);
+               damage = 10;
+                break;
+                default:
+
+                    break;
+         }
+     return damage;
     }
 
     @Override
-    protected int attack() {
-        return 0;
+    protected int defense(int damage) {
+        System.out.println("Used defense skills  ");
+
+       return damage - getDefense();
     }
 
     @Override
-    protected void defense() {
-
+    protected void levelUP() {
+        setDemage(getDemage() + 5);
+        setLevel(getLevel()+ 1);
+        setDefense(getDefense()+ 5);
+        setEnergy(getEnergy() + 5);
     }
 
     @Override
