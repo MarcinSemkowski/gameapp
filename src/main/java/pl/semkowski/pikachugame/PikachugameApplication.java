9fcfc8965a1b8b573b290986f81023c3b34249dc
Marcin <marcin96semkowski@gmail.com>
2019-04-27 10:10:35 +0200

    Initial commit

---
+++ src/main/java/pl/semkowski/pikachugame/PikachugameApplication.java
@@ -0,0 +1,13 @@
+package pl.semkowski.pikachugame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PikachugameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PikachugameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/semkowski/pikachugame/PikachugameApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.semkowski.pikachugame;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PikachugameApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
